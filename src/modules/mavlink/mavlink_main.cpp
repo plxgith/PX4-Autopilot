5f13b2906a47f43d8a5cfa7565f06a5b662be8d5
Matthias Grob <maetugr@gmail.com>
2023-12-01 11:45:00 +0100

    mavlink streams: add gimbal orientation feedback to normal stream
    It only publishes when the information is available on uORB
    and is useful for the groundstation to show the gimbal's status.

--- src/modules/mavlink/mavlink_main.cpp
+++ src/modules/mavlink/mavlink_main.cpp
@@ -1512,6 +1512,7 @@ Mavlink::configure_streams_to_default(const char *configure_single_stream)
 		configure_stream_local("HOME_POSITION", 0.5f);
 		configure_stream_local("HYGROMETER_SENSOR", 0.1f);
 		configure_stream_local("LOCAL_POSITION_NED", 1.0f);
+		configure_stream_local("MOUNT_ORIENTATION", 10.0f);
 		configure_stream_local("NAV_CONTROLLER_OUTPUT", 1.0f);
 		configure_stream_local("OBSTACLE_DISTANCE", 1.0f);
 		configure_stream_local("ORBIT_EXECUTION_STATUS", 2.0f);
