d7b96b04e298b3e828ab7a82c4f4ccac890d8508
ttropcic <tomislav.tropcic@gmail.com>
2025-10-09 14:18:40 +0200

    Fix some outdated Mavlink messages - SET_ACTUATOR_TARGET is for offboard? - MAV_TYPE_VTOL_RESERVED4 should be replaced with MAV_TYPE_VTOL_RESERVED5 - smart battery shit doesn't matter anyway

--- src/modules/mavlink/streams/SMART_BATTERY_INFO.hpp
+++ src/modules/mavlink/streams/SMART_BATTERY_INFO.hpp
@@ -42,14 +42,14 @@ public:
 	static MavlinkStream *new_instance(Mavlink *mavlink) { return new MavlinkStreamSmartBatteryInfo(mavlink); }
 
 	static constexpr const char *get_name_static() { return "SMART_BATTERY_INFO"; }
-	static constexpr uint16_t get_id_static() { return MAVLINK_MSG_ID_SMART_BATTERY_INFO; }
+	static constexpr uint16_t get_id_static() { return MAVLINK_MSG_ID_BATTERY_INFO; }
 
 	const char *get_name() const override { return get_name_static(); }
 	uint16_t get_id() override { return get_id_static(); }
 
 	unsigned get_size() override
 	{
-		static constexpr unsigned size_per_battery = MAVLINK_MSG_ID_SMART_BATTERY_INFO_LEN + MAVLINK_NUM_NON_PAYLOAD_BYTES;
+		static constexpr unsigned size_per_battery = MAVLINK_MSG_ID_BATTERY_INFO_LEN + MAVLINK_NUM_NON_PAYLOAD_BYTES;
 		return size_per_battery * _battery_status_subs.advertised_count();
 	}
 
@@ -71,11 +71,11 @@ private:
 					continue;
 				}
 
-				mavlink_smart_battery_info_t msg{};
+				mavlink_battery_info_t msg{};
 
 				msg.id = battery_status.id - 1;
-				msg.capacity_full_specification = battery_status.capacity;
-				msg.capacity_full = (int32_t)((float)(battery_status.state_of_health * battery_status.capacity) / 100.f);
+				msg.full_charge_capacity = battery_status.capacity;
+				msg.design_capacity = (int32_t)((float)(battery_status.state_of_health * battery_status.capacity) / 100.f);
 				msg.cycle_count = battery_status.cycle_count;
 
 				if (battery_status.manufacture_date) {
@@ -96,7 +96,7 @@ private:
 				msg.charging_minimum_voltage = -1;
 				msg.resting_minimum_voltage = -1;
 
-				mavlink_msg_smart_battery_info_send_struct(_mavlink->get_channel(), &msg);
+				mavlink_msg_battery_info_send_struct(_mavlink->get_channel(), &msg);
 				updated = true;
 			}
 		}
