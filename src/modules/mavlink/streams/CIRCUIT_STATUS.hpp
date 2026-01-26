c21f0ac22ec36fab9bbbb5d093fec2f10b535da9
TinKendel-PlanetIX <tin.kendjel@planetix.eu>
2026-01-26 09:17:27 +0100

    Add the rest of CIRCUIT_STATUS.hpp

--- src/modules/mavlink/streams/CIRCUIT_STATUS.hpp
+++ src/modules/mavlink/streams/CIRCUIT_STATUS.hpp
@@ -9,15 +9,14 @@ public:
 	static MavlinkStream *new_instance(Mavlink *mavlink) { return new MavlinkStreamCircuitStatus(mavlink); }
 
 	static constexpr const char *get_name_static() { return "CIRCUIT_STATUS"; }
-	static constexpr uint16_t get_id_static() { return 0;/*TODO[TIN]*/ }
+	static constexpr uint16_t get_id_static() { return MAVLINK_MSG_ID_CIRCUIT_STATUS; }
 
 	const char *get_name() const override { return get_name_static(); }
 	uint16_t get_id() override { return get_id_static(); }
 
 	unsigned get_size() override
 	{
-		;
-		//TODO[TIN]
+		return _circuit_status_subs.advertised_count() * (MAVLINK_MSG_ID_CIRCUIT_STATUS_LEN + MAVLINK_NUM_NON_PAYLOAD_BYTES);
 	}
 
 private:
@@ -29,7 +28,20 @@ private:
 	{
 		bool update = false;
 
-		//TODO[TIN]
+		for (int i = 0; i < _circuit_status_subs.size(); i++) {
+			circuit_status_s circuit_status;
+
+			if (_circuit_status_subs[i].update(&circuit_status)) {
+				mavlink_circuit_status_t msg {};
+				msg.id = i;
+				msg.voltage = circuit_status.voltage;
+				msg.current = circuit_status.current;
+
+				mavlink_msg_circuit_status_send_struct(_mavlink->get_channel(), &msg);
+
+				update = true;
+			}
+		}
 	}
 };
 
