a20dba24b30a75fad4aa1eb472a359c0cee839d4
ttropcic <tomislav.tropcic@gmail.com>
2026-04-13 11:43:17 +0200

    Update motor_test with start delay

--- src/systemcmds/motor_test/motor_test.cpp
+++ src/systemcmds/motor_test/motor_test.cpp
@@ -5,9 +5,9 @@
  ****************************************************************************/
 
 /**
- * @file motor_test.c
+ * @file motor_test.cpp
  *
- * Tool for drive testing (extended)
+ * Tool for drive testing (extended, smooth sine, safe timeout)
  */
 
 #include <drivers/drv_hrt.h>
@@ -71,86 +71,94 @@ static void motor_test(unsigned channel, float value, uint8_t driver_instance, i
 static int motor_test_worker(int argc, char *argv[])
 {
 	hrt_abstime start = 0;
+	bool was_running = false;
+
+	const int update_hz = 5000; // 5 kHz update rate for smooth sine
+	const int update_us = 1000000 / update_hz;
 
 	while (true) {
 
 		if (!g_running) {
+			// stop motors if we just transitioned from running
+			if (was_running) {
+				if (g_channel < 0) {
+					for (int i = 0; i < 8; ++i) motor_test(i, -1.f, g_driver, 0);
+				} else {
+					motor_test(g_channel, -1.f, g_driver, 0);
+				}
+				PX4_INFO("motors stopped");
+				start = 0;
+			}
+			was_running = false;
 			px4_usleep(10000);
 			continue;
 		}
 
-		if (start == 0) {
-			start = hrt_absolute_time();
-		}
+		was_running = true;
 
+		if (start == 0) start = hrt_absolute_time();
 		hrt_abstime now = hrt_absolute_time();
 
-		/* timeout */
-		if (g_timeout_ms > 0 &&
-		    (now - start) >= (hrt_abstime)g_timeout_ms * 1000) {
+		// timeout
+		if (g_timeout_ms > 0 && (now - start) >= (hrt_abstime)g_timeout_ms * 1000) {
 			g_running = false;
 			g_mode = TestMode::NONE;
+			continue; // next loop will stop motors
 		}
+
+		float val = 0.f;
 
 		switch (g_mode) {
 
 		case TestMode::STATIC:
-
-			if (g_channel < 0) {
-				for (int i = 0; i < 8; ++i) {
-					motor_test(i, g_value, g_driver, 0);
-				}
-			} else {
-				motor_test(g_channel, g_value, g_driver, 0);
-			}
-
-			px4_usleep(10000);
+			val = g_value;
 			break;
 
 		case TestMode::MIN_MAX: {
-
-			static bool high = false;
-			const int half_period_us = (int)(1e6f / g_freq / 2.f);
-			float val = high ? g_max : g_min;
-
-			if (g_channel < 0) {
-				for (int i = 0; i < 8; ++i) {
-					motor_test(i, val, g_driver, 0);
-				}
+			double t = (now - start) / 1e6; // seconds
+			double period = 1.0 / g_freq;
+			double phase = fmod(t, period) / period;
+			// linear ramp up/down for smoother square wave
+			if (phase < 0.5) {
+				val = g_min + (g_max - g_min) * (phase * 2.0);
 			} else {
-				motor_test(g_channel, val, g_driver, 0);
+				val = g_max - (g_max - g_min) * ((phase - 0.5) * 2.0);
 			}
-
-			high = !high;
-			px4_usleep(half_period_us);
 			break;
 		}
 
 		case TestMode::SINE: {
+			double t = (now - start) / 1e6; // seconds
 
-			float t = (now - start) / 1e6f;
-			float amp = (g_max - g_min) * 0.5f;
-			float offset = g_min + amp;
-			float val = offset + amp * sinf(2.f * M_PI_F * g_freq * t);
-
-			if (g_channel < 0) {
-				for (int i = 0; i < 8; ++i) {
-					motor_test(i, val, g_driver, 0);
-				}
+			// hold minimum value for first 3 seconds
+			if (t < 3.0) {
+				val = g_min;
 			} else {
-				motor_test(g_channel, val, g_driver, 0);
+				double t_shifted = t - 3.0; // start sine after delay
+				double amp = (g_max - g_min) * 0.5;
+				double offset = g_min + amp;
+				val = offset + amp * sin(2.0 * M_PI * g_freq * t_shifted);
 			}
-
-			px4_usleep(1000);
 			break;
 		}
 
 		case TestMode::NONE:
 		default:
-			start = 0;
-			px4_usleep(10000);
+			val = -1.f;
 			break;
 		}
+
+		// apply value to selected channels
+		if (g_channel < 0) {
+			for (int i = 0; i < 8; ++i) motor_test(i, val, g_driver, 0);
+		} else {
+			motor_test(g_channel, val, g_driver, 0);
+		}
+
+		// high-resolution sleep
+		hrt_abstime elapsed = hrt_absolute_time() - now;
+		int dt = update_us - (int)elapsed;
+		if (dt > 0) px4_usleep(dt);
 	}
 
 	return 0;
@@ -162,14 +170,15 @@ static int motor_test_worker(int argc, char *argv[])
 
 static void start_worker()
 {
-	if (g_task >= 0) {
-		return;
-	}
+	if (g_task >= 0) return;
+	g_mode = TestMode::NONE;
+	px4_usleep(10000);
+
 
 	g_task = px4_task_spawn_cmd("motor_test_worker",
 				    SCHED_DEFAULT,
 				    SCHED_PRIORITY_DEFAULT,
-				    2000,
+				    3000,
 				    motor_test_worker,
 				    nullptr);
 }
@@ -180,13 +189,11 @@ static void start_worker()
 
 static void usage(const char *reason)
 {
-	if (reason) {
-		PX4_WARN("%s", reason);
-	}
+	if (reason) PX4_WARN("%s", reason);
 
 	PRINT_MODULE_DESCRIPTION(
 		R"DESCR_STR(
-Motor test utility (extended).
+Motor test utility (smooth sine, safe timeout).
 
 WARNING: remove all props before using.
 )DESCR_STR");
@@ -194,7 +201,7 @@ WARNING: remove all props before using.
 	PRINT_MODULE_USAGE_NAME("motor_test", "command");
 
 	PRINT_MODULE_USAGE_COMMAND_DESCR("test", "Static motor value");
-	PRINT_MODULE_USAGE_COMMAND_DESCR("test_min_max", "Toggle between min/max");
+	PRINT_MODULE_USAGE_COMMAND_DESCR("test_min_max", "Toggle between min/max with ramp");
 	PRINT_MODULE_USAGE_COMMAND_DESCR("test_sine", "Sine wave test");
 	PRINT_MODULE_USAGE_COMMAND_DESCR("stop", "Stop all motors");
 
@@ -225,9 +232,7 @@ int motor_test_main(int argc, char *argv[])
 	const char *myoptarg = nullptr;
 
 	while ((ch = px4_getopt(argc, argv, "i:m:p:t:l:h:f:", &myoptind, &myoptarg)) != EOF) {
-
 		switch (ch) {
-
 		case 'i': driver = atoi(myoptarg); break;
 		case 'm': channel = atoi(myoptarg) - 1; break;
 		case 'p': value = atof(myoptarg) / 100.f; break;
@@ -235,7 +240,6 @@ int motor_test_main(int argc, char *argv[])
 		case 'h': max_value = atof(myoptarg) / 100.f; break;
 		case 'f': frequency = atof(myoptarg); break;
 		case 't': timeout_ms = atoi(myoptarg) * 1000; break;
-
 		default:
 			usage(nullptr);
 			return 1;
@@ -249,69 +253,52 @@ int motor_test_main(int argc, char *argv[])
 
 	const char *cmd = argv[myoptind];
 
-	/* STOP */
 	if (strcmp(cmd, "stop") == 0) {
-
 		g_running = false;
 		g_mode = TestMode::NONE;
-
-		for (int i = 0; i < 8; ++i) {
-			motor_test(i, -1.f, driver, 0);
-		}
-
+		for (int i = 0; i < 8; ++i) motor_test(i, -1.f, driver, 0);
 		return 0;
 	}
 
 	start_worker();
 
-	/* TEST */
 	if (strcmp(cmd, "test") == 0) {
-
 		g_channel = channel;
 		g_value = value;
 		g_driver = driver;
 		g_timeout_ms = timeout_ms;
-
 		g_mode = TestMode::STATIC;
 		g_running = true;
 		return 0;
 	}
 
-	/* MIN MAX */
 	if (strcmp(cmd, "test_min_max") == 0) {
-
 		if (max_value <= min_value) {
 			PX4_WARN("max must be > min");
 			return 1;
 		}
-
 		g_channel = channel;
 		g_min = min_value;
 		g_max = max_value;
 		g_freq = frequency;
 		g_driver = driver;
 		g_timeout_ms = timeout_ms;
-
 		g_mode = TestMode::MIN_MAX;
 		g_running = true;
 		return 0;
 	}
 
-	/* SINE */
 	if (strcmp(cmd, "test_sine") == 0) {
-
 		if (max_value <= min_value) {
 			PX4_WARN("max must be > min");
 			return 1;
 		}
-
 		g_channel = channel;
 		g_min = min_value;
 		g_max = max_value;
 		g_freq = frequency;
 		g_driver = driver;
 		g_timeout_ms = timeout_ms;
-
 		g_mode = TestMode::SINE;
 		g_running = true;
 		return 0;
