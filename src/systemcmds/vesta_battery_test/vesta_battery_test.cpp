06f49445ae0e8c2f8ede6458d8a7827574910d3b
ttropcic <tomislav.tropcic@gmail.com>
2026-08-18 11:10:11 +0200

    Basic starting program
    -this just compiles, shows as a system command, and prints a message

---
+++ src/systemcmds/vesta_battery_test/vesta_battery_test.cpp
@@ -0,0 +1,23 @@
+#include "vesta_battery_test.hpp"
+
+
+
+extern "C" {
+	__EXPORT int vesta_battery_test_main(int argc, char *argv[]);
+}
+
+void run_test();
+
+int vesta_battery_test_main(int argc, char *argv[])
+{
+
+	PX4_INFO("Started battery test program");
+
+	return 0;
+
+}
+
+void run_test()
+{
+
+}
---
+++ src/systemcmds/vesta_battery_test/vesta_battery_test.hpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <px4_platform_common/posix.h>
+
+
+#include <uORB/uORB.h>
+#include <uORB/topics/battery_status.h>
+
+
