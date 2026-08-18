8c5d635d3e095e7ebb0e8222891e8165a1a7cd51
ttropcic <tomislav.tropcic@gmail.com>
2026-08-18 15:32:41 +0200

    Add basic class; doesn't work

--- src/systemcmds/vesta_battery_test/vesta_battery_test.cpp
+++ src/systemcmds/vesta_battery_test/vesta_battery_test.cpp
@@ -1,23 +1,106 @@
 #include "vesta_battery_test.hpp"
 
 
-
-extern "C" {
-	__EXPORT int vesta_battery_test_main(int argc, char *argv[]);
+VestaBatteryTest::VestaBatteryTest() :
+	ModuleParams(nullptr),
+	ScheduledWorkItem(MODULE_NAME, px4::wq_configurations::test1)
+{
 }
 
-void run_test();
-
-int vesta_battery_test_main(int argc, char *argv[])
+int VestaBatteryTest::task_spawn(int argc, char *argv[])
 {
+	VestaBatteryTest *instance = new VestaBatteryTest();
 
-	PX4_INFO("Started battery test program");
+	if(instance) {
+		_object.store(instance);
+		_task_id = task_id_is_work_queue;
+
+
+		// if hardware scheduled work item ok
+		if(instance->init()) {
+			return PX4_OK;
+		}
+		else {
+			PX4_ERR("alloc failed");
+		}
+
+		delete instance;
+		_object.store(nullptr);
+		_task_id = -1;
+
+		return PX4_ERROR;
+	}
+}
+
+bool VestaBatteryTest::init()
+{
+	// run every 10ms
+	ScheduleOnInterval(interval_us);
+	return true;
+}
+
+
+int VestaBatteryTest::custom_command(int argc, char *argv[])
+{
+	return 0;
+}
+
+int VestaBatteryTest::print_usage(const char *reason)
+{
+	if (reason) {
+		PX4_WARN("%s\n", reason);
+	}
+
+	PRINT_MODULE_DESCRIPTION(
+		R"DESCR_STR(
+### Description
+Example of a simple module running out of a work queue.
+
+)DESCR_STR");
+
+	PRINT_MODULE_USAGE_NAME("work_item_example", "template");
+	PRINT_MODULE_USAGE_COMMAND("start");
+	PRINT_MODULE_USAGE_DEFAULT_COMMANDS();
 
 	return 0;
-
 }
 
-void run_test()
+void VestaBatteryTest::Run()
 {
+	PX4_INFO("Vesta Battery Test Run Start");
+	if(should_exit()) {
+		ScheduleClear();
+		exit_and_cleanup();
+		return;
+	}
+
+	// if new battery data
+	if(_battery_sub.updated()) {
+		battery_status_s battery;
+
+		if(_battery_sub.copy(&battery)){
+
+			// Adjust throttle based on current
+
+		}
+
+
+
+	}
+	time_t test_time = hrt_absolute_time();
+
+	if(test_time - _last_time > 1e6) {
+		PX4_INFO("Now");
+	}
+
 
 }
+
+
+
+extern "C" __EXPORT int vesta_battery_test_main(int argc, char *argv[])
+{
+	return VestaBatteryTest::main(argc, argv);
+}
+
+
--- src/systemcmds/vesta_battery_test/vesta_battery_test.hpp
+++ src/systemcmds/vesta_battery_test/vesta_battery_test.hpp
@@ -1,8 +1,39 @@
 #include <stdio.h>
 #include <px4_platform_common/posix.h>
+#include <px4_platform_common/defines.h>
 
 
 #include <uORB/uORB.h>
+#include <uORB/Subscription.hpp>
 #include <uORB/topics/battery_status.h>
+#include <uORB/topics/debug_array.h>
+
+#include <px4_platform_common/module.h>
+#include <px4_platform_common/module_params.h>
+
+#include <px4_platform_common/px4_work_queue/ScheduledWorkItem.hpp>
 
 
+class VestaBatteryTest : public ModuleBase<VestaBatteryTest>, public ModuleParams, public px4::ScheduledWorkItem
+{
+public:
+	VestaBatteryTest();
+	~VestaBatteryTest() override = default;
+
+	static int task_spawn(int argc, char *argv[]);
+	static int custom_command(int argc, char *argv[]);
+	static int print_usage(const char *reason = nullptr);
+
+	bool init();
+
+private:
+	static constexpr double interval_us = 10000;	// 10ms; 100Hz
+
+	void Run() override;
+
+	time_t _last_time;
+	// Battery Subscription
+	uORB::Subscription _battery_sub{ORB_ID(battery_status)};
+
+
+};
